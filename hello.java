c192b2157b3401d84e7a82ec2d0f9636ddbab65a
David Friday <davidsfriday13@gmail.com>
2019-10-07 19:14:36 -0500

    hello world java first commit

---
+++ hello.java
@@ -0,0 +1,16 @@
+
+public class hello {
+    // note public class XX 
+    // XX === name of file
+    public static void main(String args[]){
+        // this is the mian method and is where java starts to run.
+        // all other function needs to run in this
+        System.out.println("hello World");
+        // to run code form terminal type javac (this means java compiler)
+        // then name of file example (hello.java)
+        // if mistakes it will show up here 
+        // note this makes a .class file that is based off of the .java file
+        // then we need to do java [name of file] (java hello too run the file)
+
+    }
+}
